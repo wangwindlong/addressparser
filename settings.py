1dae3436e2f5182cb82e48e5516fd54242d52b00
yunlong782 <yunlong782@126.com>
2020-05-08 21:15:51 +0800

    适配公司的地址数据库

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ REDIS_URL = 'redis://root@212.64.35.132:6379'
 DATABASE = {'drivername': 'mysql',
             'host': '134.175.151.115',
             'port': '3306',
-            'username': 'yxgtest',
-            'password': 'dongjunyao',
+            'username': 'xxx',
+            'password': 'xxx',
             'database': 'app_yxg',
             'query': {'charset': 'utf8'}}
